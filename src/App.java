ff805df994c74364e94ab8061e5a870ff5d0195c
Tuan Trinh <tuan882612@gmail.com>
2022-08-09 09:34:07 -0500

    part 2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
-public class App {
-    public static void main(String[] args){
-        System.out.println("Test");
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App implements ActionListener {
+    private final JFrame mainFrame;
+
+    public App() {
+        mainFrame = new JFrame("Character Generator");
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainFrame.setSize(1000, 1000);
+
+        mainFrame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+
     }
 }
---
+++ src/index.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class index {
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(App::new);
+    }
+}
